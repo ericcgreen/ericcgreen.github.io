d9d6d3af16a46ca98933a08be2142c48c6ac99ed
Eric Christopher Green <VERDE611@GMAIL.COM>
2017-03-20 00:13:53 -0400

    added welcome path

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ angular
 function RouterFunction($stateProvider){
   $stateProvider
   .state("welcome", {
-    url: "/",
+    url: "/welcome",
     templateUrl: "js/ng-views/welcome.html",
     controller: "WelcomeCtrl",
     controllerAs: "vm"
