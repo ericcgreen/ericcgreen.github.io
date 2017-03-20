36a1be40ea3394b1037511979e201d75170f3bb3
Eric Christopher Green <VERDE611@GMAIL.COM>
2017-03-20 08:18:59 -0400

    moved welcome page into index.html

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ angular
 function RouterFunction($stateProvider){
   $stateProvider
   .state("welcome", {
-    url: "/welcome",
+    url: "/",
     templateUrl: "js/ng-views/welcome.html",
     controller: "WelcomeCtrl",
     controllerAs: "vm"
